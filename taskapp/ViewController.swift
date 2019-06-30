3bbc48e043c61060a046fa29ef3e6a61946f9f57
daichi987 <masashi.y1028@gmail.com>
2019-06-30 23:18:11 +0900

    Initial Commit

---
+++ taskapp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  taskapp
+//
+//  Created by Mika on 2019/06/30.
+//  Copyright © 2019 daichi987. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
